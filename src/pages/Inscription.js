38ba00f74234a761f8435bcabfd778c024846e1f
Jolly KETA <vincentiaketamaboudi@gmail.com>
2025-03-04 21:13:42 +0100

    Ajout du header avec video en arriere plan

--- src/pages/Inscription.js
+++ src/pages/Inscription.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaBook, FaEnvelope, FaPhone, FaUser } from "react-icons/fa";
 import InscriptionImg from "../assets/img/inscription.jpg";
+import BackgroundVideo from "../assets/videos/Accueil2.mp4";
 
 function Inscription() {
   const [formData, setFormData] = useState({
@@ -27,8 +28,7 @@ function Inscription() {
 
     // Regex patterns for validation
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const phonePattern = /^[0-9]{9}$/; // Adjust phone number pattern as needed
-    // Le 9 dans {9} signifie que le numéro de téléphone doit contenir 9 chiffres
+    const phonePattern = /^[0-9]{9}$/; // Regex pour 10 chiffres
 
     // Check if all fields are filled and valid
     if (
@@ -52,188 +52,212 @@ function Inscription() {
       }
 
       setMessage("Inscription réussie !");
-      console.log("Form Data:", formData); // Afficher les données du formulaire dans la console
     } else {
       setMessage("Veuillez remplir tous les champs correctement.");
     }
   };
 
   return (
-    <div className="p-10 flex items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8 bg-gray-100">
-      <main className="w-full p-8 bg-white rounded-lg shadow-lg flex flex-col lg:flex-row">
-        {/* Section de l'image à gauche */}
-        <section className="w-full lg:w-1/2 pr-0 lg:pr-8 flex items-center justify-center mb-8 lg:mb-0">
-          <img
-            src={InscriptionImg}
-            alt="Inscription"
-            className="rounded-lg shadow-lg"
-          />
-        </section>
-        {/* Section du formulaire à droite */}
-        <section className="w-full lg:w-1/2 pl-0 lg:pl-8">
-          <h2 className="mb-6 text-3xl font-extrabold text-center text-gray-800">
-            Inscription
-          </h2>
-          <p className="mb-8 text-center text-gray-600">
-            Remplissez ce formulaire pour vous inscrire.
-          </p>
+    <div>
+      {/* Header avec arriere plan en video */}
+      <header className="relative w-full overflow-hidden h-96">
+        <video
+          className="absolute top-0 left-0 object-cover w-full h-full"
+          src={BackgroundVideo}
+          autoPlay
+          loop
+          muted
+        />
+        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+          <h1 className="text-4xl font-bold text-white">
+            Inscrivez-vous des maintenant !
+          </h1>
+        </div>
+      </header>
+      {/* fin header */}
 
-          <form
-            id="inscriptionForm"
-            onSubmit={handleSubmit}
-            className="space-y-6"
-          >
-            <div className="relative">
-              <label
-                htmlFor="nom"
-                className="block text-lg font-medium text-gray-700"
-              >
-                Nom :
-              </label>
-              <div className="flex items-center mt-2">
-                <FaUser className="absolute ml-3 text-gray-400" />
-                <input
-                  type="text"
-                  id="nom"
-                  name="nom"
-                  placeholder="Votre nom"
-                  value={formData.nom}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
-            </div>
+      {/** Contenu principal */}
+      <div className="flex items-center justify-center min-h-screen p-10 px-4 bg-gray-100 sm:px-6 lg:px-8 mt-96">
+        <main className="flex flex-col w-full p-10 bg-white rounded-lg shadow-lg sm:p-20 lg:flex-row">
+          {/* Section de l'image à gauche */}
+          <section className="flex items-center justify-center w-full pr-0 mb-8 lg:w-1/2 lg:pr-8 lg:mb-0">
+            <img
+              src={InscriptionImg}
+              alt="Inscription"
+              className="rounded-lg shadow-lg"
+            />
+          </section>
 
-            <div className="relative">
-              <label
-                htmlFor="prenom"
-                className="block text-lg font-medium text-gray-700"
-              >
-                Prénom :
-              </label>
-              <div className="flex items-center mt-2">
-                <FaUser className="absolute ml-3 text-gray-400" />
-                <input
-                  type="text"
-                  id="prenom"
-                  name="prenom"
-                  placeholder="Votre prénom"
-                  value={formData.prenom}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
-            </div>
+          {/* Section du formulaire à droite */}
+          <section className="w-full pl-0 lg:w-1/2 lg:pl-8">
+            <h2 className="mb-6 text-3xl font-extrabold text-center text-gray-800">
+              Inscription
+            </h2>
+            <p className="mb-8 text-center text-gray-600">
+              Remplissez ce formulaire pour vous inscrire.
+            </p>
 
-            <div className="relative">
-              <label
-                htmlFor="email"
-                className="block text-lg font-medium text-gray-700"
-              >
-                Email :
-              </label>
-              <div className="flex items-center mt-2">
-                <FaEnvelope className="absolute ml-3 text-gray-400" />
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Votre email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
-            </div>
-
-            <div className="relative">
-              <label
-                htmlFor="telephone"
-                className="block text-lg font-medium text-gray-700"
-              >
-                Téléphone :
-              </label>
-              <div className="flex items-center mt-2">
-                <FaPhone className="absolute ml-3 text-gray-400" />
-                <input
-                  type="tel"
-                  id="telephone"
-                  name="telephone"
-                  placeholder="Ex: 061234567"
-                  value={formData.telephone}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
-            </div>
-
-            <div className="relative">
-              <label
-                htmlFor="formation"
-                className="block text-lg font-medium text-gray-700"
-              >
-                Formation souhaitée :
-              </label>
-              <div className="flex items-center mt-2">
-                <FaBook className="absolute ml-3 text-gray-400" />
-                <select
-                  id="formation"
-                  name="formation"
-                  value={formData.formation}
-                  onChange={handleChange}
-                  required
-                  className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            <form
+              id="inscriptionForm"
+              onSubmit={handleSubmit}
+              className="space-y-6"
+            >
+              <div className="relative">
+                <label
+                  htmlFor="nom"
+                  className="block text-lg font-medium text-gray-700"
                 >
-                  <option value="">Sélectionnez une formation</option>
-                  <option value="dev_web">Développement Web</option>
-                  <option value="reseaux">Réseaux et Sécurité</option>
-                  <option value="marketing">Marketing Digital</option>
-                </select>
+                  Nom :
+                </label>
+                <div className="flex items-center mt-2">
+                  <FaUser className="absolute ml-3 text-gray-400" />
+                  <input
+                    type="text"
+                    id="nom"
+                    name="nom"
+                    placeholder="Votre nom"
+                    value={formData.nom}
+                    onChange={handleChange}
+                    required
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="conditions"
-                name="conditions"
-                checked={formData.conditions}
-                onChange={handleChange}
-                required
-                className="mr-2"
-              />
-              <label className="text-gray-700">
-                J&apos;accepte les{" "}
-                <button type="button" className="text-blue-500 hover:underline">
-                  conditions générales
+              <div className="relative">
+                <label
+                  htmlFor="prenom"
+                  className="block text-lg font-medium text-gray-700"
+                >
+                  Prénom :
+                </label>
+                <div className="flex items-center mt-2">
+                  <FaUser className="absolute ml-3 text-gray-400" />
+                  <input
+                    type="text"
+                    id="prenom"
+                    name="prenom"
+                    placeholder="Votre prénom"
+                    value={formData.prenom}
+                    onChange={handleChange}
+                    required
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+              </div>
+
+              <div className="relative">
+                <label
+                  htmlFor="email"
+                  className="block text-lg font-medium text-gray-700"
+                >
+                  Email :
+                </label>
+                <div className="flex items-center mt-2">
+                  <FaEnvelope className="absolute ml-3 text-gray-400" />
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Votre email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+              </div>
+
+              <div className="relative">
+                <label
+                  htmlFor="telephone"
+                  className="block text-lg font-medium text-gray-700"
+                >
+                  Téléphone :
+                </label>
+                <div className="flex items-center mt-2">
+                  <FaPhone className="absolute ml-3 text-gray-400" />
+                  <input
+                    type="tel"
+                    id="telephone"
+                    name="telephone"
+                    placeholder="Ex: 0612345678"
+                    value={formData.telephone}
+                    onChange={handleChange}
+                    required
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+              </div>
+
+              <div className="relative">
+                <label
+                  htmlFor="formation"
+                  className="block text-lg font-medium text-gray-700"
+                >
+                  Formation souhaitée :
+                </label>
+                <div className="flex items-center mt-2">
+                  <FaBook className="absolute ml-3 text-gray-400" />
+                  <select
+                    id="formation"
+                    name="formation"
+                    value={formData.formation}
+                    onChange={handleChange}
+                    required
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    <option value="">Sélectionnez une formation</option>
+                    <option value="dev_web">Développement Web</option>
+                    <option value="reseaux">Réseaux et Sécurité</option>
+                    <option value="marketing">Marketing Digital</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="conditions"
+                  name="conditions"
+                  checked={formData.conditions}
+                  onChange={handleChange}
+                  required
+                  className="mr-2"
+                />
+                <label className="text-gray-700">
+                  J&apos;accepte les{" "}
+                  <button
+                    type="button"
+                    className="text-blue-500 hover:underline"
+                    onClick={() => alert("Conditions générales")}
+                  >
+                    conditions générales
+                  </button>
+                </label>
+              </div>
+
+              <div className="text-center">
+                <button
+                  type="submit"
+                  className="w-full py-3 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                  S&apos;inscrire
                 </button>
-              </label>
-            </div>
+              </div>
+            </form>
 
-            <div className="text-center">
-              <button
-                type="submit"
-                className="w-full py-3 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >
-                S&apos;inscrire
-              </button>
-            </div>
-          </form>
-
-          <p
-            id="message"
-            className={`mt-4 text-center ${
-              message.includes("réussie") ? "text-green-500" : "text-red-500"
-            }`}
-          >
-            {message}
-          </p>
-        </section>
-      </main>
+            <p
+              id="message"
+              className={`mt-4 text-center ${
+                message.includes("réussie") ? "text-green-500" : "text-red-500"
+              }`}
+            >
+              {message}
+            </p>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
