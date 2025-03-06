69892b9e08cdea1c47a6f3d5d052c83dc2dde071
Jolly KETA <vincentiaketamaboudi@gmail.com>
2025-03-06 05:27:43 +0100

    Ajout des routes vers Formations, Informatique, Gestion, Commerce

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ const Accueil = lazy(() => import("./pages/Accueil"));
 const Apropos = lazy(() => import("./pages/Apropos"));
 const Contact = lazy(() => import("./pages/Contact"));
 const Inscription = lazy(() => import("./pages/Inscription"));
+const Formations = lazy(() => import("./pages/formations/Formations"));
+const Informatique = lazy(() => import("./pages/formations/Informatique"));
+const Gestion = lazy(() => import("./pages/formations/Gestion"));
+const Commerce = lazy(() => import("./pages/formations/Commerce"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
@@ -21,13 +25,24 @@ function App() {
       <main>
         {/* Suspense pour le chargement paresseux des pages */}
         <Suspense
-          fallback={<div className="text-center py-6">Chargement...</div>}
+          fallback={<div className="py-6 text-center">Chargement...</div>}
         >
           <Routes>
+            {/* Page d'accueil */}
             <Route path="/" element={<Accueil />} />
+
+            {/* Pages principales */}
             <Route path="/inscription" element={<Inscription />} />
             <Route path="/apropos" element={<Apropos />} />
             <Route path="/contact" element={<Contact />} />
+
+            {/* Page Formations avec sous-onglets */}
+            <Route path="/formations" element={<Formations />}>
+              <Route path="informatique" element={<Informatique />} />
+              <Route path="gestion" element={<Gestion />} />
+              <Route path="commerce" element={<Commerce />} />
+            </Route>
+
             {/* Page d'erreur 404 pour les routes non définies */}
             <Route path="*" element={<NotFound />} />
           </Routes>
